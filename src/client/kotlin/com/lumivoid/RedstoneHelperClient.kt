a266fbb4668731603faef1bb32a70d9fd705a816
Artem <krutog727@gmail.com>
2024-03-30 14:37:13 +0200

    Add empty (almost) mod

---
+++ src/client/kotlin/com/lumivoid/RedstoneHelperClient.kt
@@ -0,0 +1,9 @@
+package com.lumivoid
+
+import net.fabricmc.api.ClientModInitializer
+
+object RedstoneHelperClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
