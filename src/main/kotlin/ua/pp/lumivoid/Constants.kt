e37aca62ec72f2446f47679dd0769e9f729fa08a
Artem <krutog727@gmail.com>
2024-06-12 01:54:08 +0300

    bump version

--- src/main/kotlin/ua/pp/lumivoid/Constants.kt
+++ src/main/kotlin/ua/pp/lumivoid/Constants.kt
@@ -5,6 +5,6 @@ import net.minecraft.util.Identifier
 
 object Constants {
     const val MOD_ID = "redstone-helper"
-    const val MOD_VERSION = "0.1.3"
+    const val MOD_VERSION = "0.1.4"
     val NET_CHANNEL: OwoNetChannel = OwoNetChannel.create(Identifier(MOD_ID, "main"))
 }
