792888fa695bceecb71ce5a315fcfe01b91fa657
Artem <krutog727@gmail.com>
2024-07-01 21:06:29 +0300

    +/autodust - redirect to autowire

--- src/main/kotlin/ua/pp/lumivoid/commands/AutoWireCommand.kt
+++ src/main/kotlin/ua/pp/lumivoid/commands/AutoWireCommand.kt
@@ -1,6 +1,7 @@
 package ua.pp.lumivoid.commands
 
 import com.mojang.brigadier.CommandDispatcher
+import com.mojang.brigadier.tree.LiteralCommandNode
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource
 import net.minecraft.client.MinecraftClient
@@ -13,14 +14,22 @@ object AutoWireCommand {
     fun register(dispatcher: CommandDispatcher<FabricClientCommandSource?>) {
         logger.debug("/autowire: Registering autowire command")
 
-        dispatcher.register(ClientCommandManager.literal("autowire")
-            .requires {source -> source.hasPermissionLevel(2)}
+        val autoWireCommandNode: LiteralCommandNode<FabricClientCommandSource?> = dispatcher.register(ClientCommandManager.literal("autowire")
+            .requires { source -> source.hasPermissionLevel(2) }
             .executes {
                 logger.debug("/autowire: Opening autowire menu")
                 MinecraftClient.getInstance().send(Runnable {
                     MinecraftClient.getInstance().setScreen(AutowireScreen())
                 })
                 1
+            }
+        )
+
+        dispatcher.register(ClientCommandManager.literal("autodust")
+            .requires { source -> source.hasPermissionLevel(2) }
+            // We can't use .redirect because https://github.com/Mojang/brigadier/issues/46 wtf mojang! 2018!!!
+            .executes { context ->
+                dispatcher.execute("autowire", context.source)
             }
         )
     }
