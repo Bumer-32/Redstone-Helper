7d1d3bbf0bf1b577cdd5b2eac04a6569503c43b1
Artem <krutog727@gmail.com>
2025-01-23 00:05:36 +0200

    remove useless auto update system

--- buildSrc/src/main/kotlin/ua/pp/lumivoid/updater/jsonData/MinecraftVersionData.kt
+++
@@ -1,9 +0,0 @@
-package ua.pp.lumivoid.updater.jsonData
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class MinecraftVersionData(
-    val version: String,
-    val stable: Boolean
-)
